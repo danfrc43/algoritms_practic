956facd02ceb2820e732ebc4294e511b7c4866ea
danfrc43 <esculap.d.97@gmail.com>
2026-03-21 17:11:43 +1000

    Переделал 125

--- 1st lab/125. Valid Palindrome.py
+++ 1st lab/125. Valid Palindrome.py
@@ -1,3 +1,19 @@
-def isPalindrome(self, s: str) -> bool:
-        new_s = "".join(filter(str.isalnum,s)).lower()
-        return new_s[:] == new_s[::-1]
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        if len(s) == 0:
+            return True
+        while l < r:
+            if s[l].lower().isalnum():
+                if s[r].lower().isalnum():
+                    if s[l].lower() == s[r].lower():
+                        l += 1
+                        r -= 1
+                    else:
+                        return False
+                else:
+                    r -= 1
+            else:
+                l += 1
+        return True
+        
